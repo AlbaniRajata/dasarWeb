17ae9700dea9227f2a1f8e4329ed75a0d44bd209
Albani Rajata Malik <albeebro002@gmail.com>
2024-04-17 17:36:50 +0700

    Pertemuan 12 - Praktikum 1 (Basic OOP)

--- pertemuan12_phpoop/abstraction.php
+++ pertemuan12_phpoop/abstraction.php
@@ -1,33 +1,38 @@
 <?php
-    interface Shape {
-        public function calculateArea();
+    abstract class Shape{
+        abstract public function calculateArea();
     }
 
-    interface Color{
-        public function getColor();
-    }
-
-    class Circle implements Shape, Color{
+    class Circle extends Shape{
         private $radius;
-        private $color;
-
-        public function __construct($radius, $color){
+        public function __construct($radius){
             $this->radius=$radius;
-            $this->color=$color;
         }
 
         public function calculateArea()
         {
-            return pi() * pow($this->radius, 2);
-        }
-
-        public function getColor(){
-            return $this->color;
+            return pi() * pow($this-> radius, 2);
         }
     }
-    $circle = new Circle(5, "Blue");
 
-    echo "Area of circle: " . $circle->calculateArea() . "<br>";
-    echo "Color of circle: " . $circle->getColor() . "<br>";
+    class Rectangle extends Shape{
+        private $width;
+        private $height;
 
+        public function __construct($width, $height) {
+            $this->width=$width;
+            $this->height=$height;
+        }
+
+        public function calculateArea()
+        {
+            return $this->width * $this->height;
+        }
+    }
+
+    $circle = new Circle(5);
+    $rectangle = new Rectangle(4,6);
+
+    echo "Area of Circle: " . $circle->calculateArea() . "<br>";
+    echo "Area of Rectangle: " . $rectangle->calculateArea() . "<br>";
 ?>
